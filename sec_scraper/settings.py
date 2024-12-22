280f8d3d9d5679905e3d491e354c0b891793d69a
Sai Ram Pasupuleti <142623598+sairamcsep076@users.noreply.github.com>
2024-12-22 23:07:20 +0530

    deploy db

--- sec_scraper/settings.py
+++ sec_scraper/settings.py
@@ -80,11 +80,10 @@ WSGI_APPLICATION = 'sec_scraper.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Store database in a file
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),  # Store the DB in /tmp directory
     }
 }
 
