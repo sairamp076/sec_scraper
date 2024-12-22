ca1743656f669f478bad63615171dee8ab170519
Sai Ram Pasupuleti <142623598+sairamcsep076@users.noreply.github.com>
2024-12-22 22:52:15 +0530

    update wshi

--- sec_scraper/wsgi.py
+++ sec_scraper/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sec_scraper.settings')
 
 application = get_wsgi_application()
+
+# Alias 'application' as 'app' for Vercel
+app = application
